82e4c3bd9c2fee739235087f35c7f8e93649a82c
Aman Agarwal <aman.agarwal@inmobi.com>
2021-08-25 12:28:56 +0530

    zerodha login changes

--- src/server/server.go
+++ src/server/server.go
@@ -1,8 +1,13 @@
 package server
 
+import (
+	"stockify-api/src/zerodha"
+)
+
 const PORT = ":80"
 
 func Init() {
 	r := NewRouter()
+	zerodha.Init()
 	r.Run(PORT)
 }
---
+++ src/zerodha/zerodha.go
@@ -0,0 +1,46 @@
+package zerodha
+
+import (
+	"fmt"
+	kiteconnect "github.com/zerodha/gokiteconnect/v4"
+)
+
+const (
+	apiKey    string = "3ag3ura5bykyu8a5"
+	apiSecret string = "g8du8pu39k86ubqwktuwtkpjrg2ce8p2"
+)
+
+func GetAccessToken(client *kiteconnect.Client) string {
+	if client != nil {
+		fmt.Println(client.GetLoginURL())
+		requestToken := "aUdk5fc3mAvrz8czJt4S1U1HpVyqnZQy"
+		// Get user details and access token
+		data, err := client.GenerateSession(requestToken, apiSecret)
+		if err != nil {
+			fmt.Printf("Error: %v", err)
+			return ""
+		} else {
+			return data.AccessToken
+		}
+	} else {
+		return "YsQ3Nw9Uf8yA2TbJtmQfQKYHo3m8Kw7P"
+	}
+}
+
+func Login(){
+	// Create a new Kite connect instance
+	kc := kiteconnect.New(apiKey)
+	token := GetAccessToken(nil)//to generate new token pass kc instead of nil
+	kc.SetAccessToken(token)
+
+	// Get margins
+	margins, err := kc.GetUserMargins()
+	if err != nil {
+		fmt.Printf("Error getting margins: %v", err)
+	}
+	fmt.Println("margins: ", margins)
+}
+
+func Init(){
+ Login()
+}
